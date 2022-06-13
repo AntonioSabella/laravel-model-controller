3253545acda44909a20449398ed94cc3bbd0e767
AntonioSabella <asabella6@gmail.com>
2022-06-13 19:08:18 +0200

    Add style adjusts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,10 @@
             <div class="row row-cols-5 gy-2">
                 @forelse($movies as $movie)
                 <div class="col text-center bg-danger">
-                    <div class="card bg-primary">
+                    <div class="card bg-info">
                         <h4 class='bg-warning'>{{ $movie->title }}</h4>
                         <img src="https://picsum.photos/200/" alt="{{ $movie->title }}">
-                        <h5>{{ $movie->original_title }}</h5>
+                        <h5>"{{ $movie->original_title }}"</h5>
                         <p><strong>Nationality:</strong> {{ $movie->nationality}}</p>
                         <p><strong>Release Date:</strong> {{ $movie->date }}</span></p>
                         <p><strong>Movie vote:</strong> {{ $movie->vote }}</span></p>
@@ -23,8 +23,8 @@
                     <div class="col">
                     <p>No movies to show</p>
                     </div>
+                @endforelse
             </div>
-            @endforelse
         </div>
     </div>
 @endsection
